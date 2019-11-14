95a73674e27131d68ff3a097020b86484a5c14f8
SVARusu <alexandru.rusu@softvision.ro>
2019-11-14 10:49:19 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { LeftNavComponent } from './navbar/left-nav/left-nav.component';
-import { RightNavComponent } from './navbar/right-nav/right-nav.component';
 import { HeaderComponent } from './header/header.component';
 import { BodyComponent } from './body/body.component';
 import { FooterComponent } from './footer/footer.component';
